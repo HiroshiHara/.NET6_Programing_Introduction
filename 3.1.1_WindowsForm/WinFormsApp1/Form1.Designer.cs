cca5cf1006697608f301195db620bfaaf2909961
HiroshiHara <mrbob403@gmail.com>
2022-10-18 15:13:07 +0900

    add Label, TextBox and Buttons

--- 3.1.1_WindowsForm/WinFormsApp1/Form1.Designer.cs
+++ 3.1.1_WindowsForm/WinFormsApp1/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(161, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello, .NET6 World.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(104, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(341, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hello";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(104, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(341, 57);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Win32 API";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(341, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(559, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
     }
 }
--- 3.1.1_WindowsForm/WinFormsApp1/Form1.cs
+++ 3.1.1_WindowsForm/WinFormsApp1/Form1.cs
@@ -2,9 +2,15 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        // 画面を操作した時のイベント処理などを記述するファイル
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
