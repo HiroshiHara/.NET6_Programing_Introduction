c40300649d69811986b9f351830bece065e54ac7
HiroshiHara <mrbob403@gmail.com>
2022-10-21 11:24:37 +0900

    Enveded user control to winform.

---
+++ 3.3.1.WindowsFormControl/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new WinFormsControlLibrary1.UserControl1();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(54, 40);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(409, 330);
+            this.userControl11.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(487, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 75);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ログインフォームのサンプルです。\r\nユーザー名は任意の文字列、\r\nパスワードはAAAです。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WinFormsControlLibrary1.UserControl1 userControl11;
+        private Label label1;
+    }
+}
