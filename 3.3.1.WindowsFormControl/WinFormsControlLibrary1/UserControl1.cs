3e66117e1873c99c7c9b2a76d7339ae0df0596cf
HiroshiHara <mrbob403@gmail.com>
2022-10-21 11:43:27 +0900

    Create login form by winfrom.

--- 3.3.1.WindowsFormControl/WinFormsControlLibrary1/UserControl1.cs
+++ 3.3.1.WindowsFormControl/WinFormsControlLibrary1/UserControl1.cs
@@ -6,5 +6,38 @@
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show(
+                    "ユーザー名またはパスワードが空欄です。",
+                    ".NET6の解説",
+                     MessageBoxButtons.OK,
+                     MessageBoxIcon.Information
+                );
+                return;
+            }
+            if (textBox2.Text == "AAA")
+            {
+                MessageBox.Show(
+                    $"こんにちわ、{textBox1.Text}さん。",
+                    ".NET6の解説",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+            else
+            {
+                MessageBox.Show(
+                    $"パスワードが間違っています。",
+                    ".NET6の解説",
+                    MessageBoxButtons.RetryCancel,
+                    MessageBoxIcon.Error
+                );
+            }
+            return;
+        }
     }
 }
