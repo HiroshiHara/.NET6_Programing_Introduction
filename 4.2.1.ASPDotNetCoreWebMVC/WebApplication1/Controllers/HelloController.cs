dd4c3f7deb4a0a135e61a21db86eb37edacb74f0
HiroshiHara <mrbob403@gmail.com>
2022-11-12 19:57:51 +0900

    新規MVCを追加

---
+++ 4.2.1.ASPDotNetCoreWebMVC/WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class HelloController : Controller
+	{
+		public IActionResult Index()
+		{
+			var model = new HelloModel();
+			model.Name = "Hiroshi Hara";
+			model.Now = DateTime.Now;
+			return View(model);
+		}
+	}
+}
---
+++ 4.2.1.ASPDotNetCoreWebMVC/WebApplication1/Models/HelloModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+	public class HelloModel
+	{
+		public string? Name { get; set; }
+		public DateTime Now { get; set; }
+	}
+}
